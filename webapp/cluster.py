c3a7f62c130a4c4d51ba37b1a36a2d681f8451fe
NGrogs <ngrogan15@gmail.com>
2019-02-21 22:56:53 +0000

    Pushing static and image analysis files

---
+++ webapp/cluster.py
@@ -0,0 +1,40 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+
+def cluster(imgName):
+
+	path = "static/user_xrays/" #input dir
+	outputPath = "static/user_xrays/" #output dir dont forget backslash
+
+	imgpath = path + imgName
+
+	img = cv2.imread(imgpath, 1)
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+	Z = img.reshape((-1,3))
+	Z = np.float32(Z)
+
+	criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+
+	# Change the value of K to change number of clusters
+	K=8
+
+	#print(path + imgpath)
+
+	ret, label1, center1 = cv2.kmeans(Z, K, None,
+									  criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+	center1 = np.uint8(center1)
+	res1 = center1[label1.flatten()]
+	output = res1.reshape((img.shape))
+
+	output = [img, output]
+	titles = ['Original Image', imgName + ' K = ' + str(K)]
+
+
+	plt.imshow(output[1], aspect='auto')
+	plt.xticks([])
+	plt.yticks([])
+	plt.savefig(outputPath + imgName)
---
+++ webapp/new_test.py
@@ -0,0 +1,54 @@
+# classify_image.py
+
+import os
+import tensorflow as tf
+import numpy as np
+import cv2
+
+# module-level variables ##############################################################################################
+RETRAINED_LABELS_TXT_FILE_LOC = os.getcwd() + "\\" + "retrained_labels.txt"
+RETRAINED_GRAPH_PB_FILE_LOC = os.getcwd() + "\\" + "retrained_graph.pb"
+TEST_IMAGES_DIR = os.getcwd() + "static\\user_xrays"
+
+def classify_image(image_path, filename):
+
+    classifications = ['NORMAL', 'PNEUMONIA']
+
+    with tf.gfile.FastGFile(RETRAINED_GRAPH_PB_FILE_LOC, 'rb') as retrainedGraphFile:
+        # instantiate a GraphDef object
+        graphDef = tf.GraphDef()
+        # read in retrained graph into the GraphDef object
+        graphDef.ParseFromString(retrainedGraphFile.read())
+        # import the graph into the current default Graph, note that we don't need to be concerned with the return value
+        _ = tf.import_graph_def(graphDef, name='')
+    # end with
+
+    with tf.Session() as sess:
+        imageFileWithPath = os.path.join(image_path, filename)
+
+        openCVImage = cv2.imread(imageFileWithPath)
+
+        finalTensor = sess.graph.get_tensor_by_name('final_result:0')
+
+        # convert the OpenCV image (numpy array) to a TensorFlow image
+
+        tfImage = np.array(openCVImage)[:, :, 0:3]
+
+        # run the network to get the predictions
+        predictions = sess.run(finalTensor, {'DecodeJpeg:0': tfImage})
+
+        # sort predictions from most confidence to least confidence
+        sortedPredictions = predictions[0].argsort()[-len(predictions[0]):][::-1]
+
+        return classifications[sortedPredictions[0]], "{0:.2f}".format(predictions[0][sortedPredictions[0]]*100)
+#######################################################################################################################
+def main():
+    print("starting program . . .")
+
+    prediction = classify_image(TEST_IMAGES_DIR, 'clustered_IM-0010-0001.jpeg', )
+
+    print(prediction)
+
+# end main
+if __name__ == "__main__":
+    main()
